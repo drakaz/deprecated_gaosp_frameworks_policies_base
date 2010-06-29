nobunobuta/android_frameworks_policies_base

Adds a reboot option to the power menu.
Conflicts:

	mid/com/android/internal/policy/impl/ShutdownThread.java
	phone/com/android/internal/policy/impl/ShutdownThread.java

mid/com/android/internal/policy/impl/GlobalActions.java
@@ -142,6 +142,21 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 },
                 // next: silent mode
                 mSilentModeToggle,
+                // next: reboot
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_reboot) {
+
+                    public void onPress() {
+                        ShutdownThread.rebootAfterDisablingRadio(mContext, true);
+                    }
+
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+		},
                 // last: power off
                 new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_power_off) {
 

phone/com/android/internal/policy/impl/GlobalActions.java
@@ -171,6 +171,22 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mSilentModeToggle,
                 // next: airplane mode
                 mAirplaneModeOn,
+                // next: reboot
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_reboot) {
+
+                    public void onPress() {
+                        ShutdownThread.rebootAfterDisablingRadio(mContext, true);
+                    }
+
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+                },
+
                 // last: power off
                 new SinglePressAction(
                         com.android.internal.R.drawable.ic_lock_power_off,

drakaz/deprecated_gaosp_frameworks_policies_base

mid/com/android/internal/policy/impl/GlobalActions.java
@@ -142,6 +142,21 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 },
                 // next: silent mode
                 mSilentModeToggle,
+                // next: reboot
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_reboot) {
+
+                    public void onPress() {
+                        ShutdownThread.reboot(mContext, true);
+                    }
+
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+		},
                 // last: power off
                 new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_power_off) {
 

phone/com/android/internal/policy/impl/GlobalActions.java
@@ -197,6 +197,22 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mSilentModeToggle,
                 // next: airplane mode
                 mAirplaneModeOn,
+                // next: reboot
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_reboot) {
+
+                    public void onPress() {
+                        ShutdownThread.reboot(mContext, true);
+                    }
+
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+                },
+
                 // last: power off
                 new SinglePressAction(
                         com.android.internal.R.drawable.ic_lock_power_off,
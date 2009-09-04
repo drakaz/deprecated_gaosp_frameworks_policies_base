c8e74df05a0665508bb94a8f6c2a8ad36dec3d61
Steve Kondik <shade@chemlab.org>
2009-09-03 20:06:33 -0400

    Adds a reboot option to the power menu.
    Conflicts:
    
    	mid/com/android/internal/policy/impl/ShutdownThread.java
    	phone/com/android/internal/policy/impl/ShutdownThread.java

--- mid/com/android/internal/policy/impl/GlobalActions.java
+++ mid/com/android/internal/policy/impl/GlobalActions.java
@@ -142,6 +142,21 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 },
                 // next: silent mode
                 mSilentModeToggle,
+                // next: reboot
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_reboot) {
+
+                    public void onPress() {
+                        ShutdownThread.reboot(mContext, true);
+                    }
+
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+		},
                 // last: power off
                 new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_power_off) {
 
