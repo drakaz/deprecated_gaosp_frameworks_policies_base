05e1df64a885fff95bf7b8e57deb5387a74e647c
kmobs <persiansown@gmail.com>
2010-07-07 16:13:09 -0500

    Fixed the previous breakage.  Now obeys config setting

--- phone/com/android/internal/policy/impl/LockScreen.java
+++ phone/com/android/internal/policy/impl/LockScreen.java
@@ -299,8 +299,9 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-    if ((keyCode == KeyEvent.KEYCODE_MENU && mMenuUnlockScreen) ||
-        (keyCode == KeyEvent.KEYCODE_DPAD_CENTER && mTrackballUnlockScreen)) {
+    if ((keyCode == KeyEvent.KEYCODE_DPAD_CENTER && mTrackballUnlockScreen) ||
+        (keyCode == KeyEvent.KEYCODE_MENU && mMenuUnlockScreen) ||
+        (keyCode == KeyEvent.KEYCODE_MENU && mEnableMenuKeyInLockScreen)) {
             mCallback.goToUnlockScreen();
         }
         return false;
